a0cc83d0d2f3a9886e0e61105aac17e790f9a8bb
xtadeushx <alekscherkasov13@gmail.com>
2023-01-08 19:27:41 +0200

    add Htag component'

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,8 +9,21 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
         <>
             <Head>
                 <title>My top</title>
+                <link
+                    rel='preconnect'
+                    href='https://fonts.googleapis.com'
+                />
+                <link
+                    rel='preconnect'
+                    href='https://fonts.gstatic.com'
+                    // crossOrigin
+                />
+                <link
+                    href='https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;600;700&display=swap'
+                    rel='stylesheet'
+                />
             </Head>
-            <Component {...pageProps} />)
+            <Component {...pageProps} />
         </>
     );
 }
