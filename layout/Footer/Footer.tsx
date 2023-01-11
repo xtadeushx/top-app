55f3941c140f533fcb51ca09d005279cf067cfa2
xtadeushx <alekscherkasov13@gmail.com>
2023-01-11 19:32:44 +0200

    add footer layout

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -2,11 +2,33 @@ import React, { FC } from 'react';
 import { IFooterProps } from './Footer.props';
 
 import styles from './Footer.module.css';
+import cn from 'classnames';
+import { format } from 'date-fns';
 
-const Footer: FC<IFooterProps> = ({ ...props }): JSX.Element => {
+const Footer: FC<IFooterProps> = ({ className, ...props }): JSX.Element => {
     return (
-        <footer {...props}>
-            <h2>Footer</h2>{' '}
+        <footer
+            className={cn(className, styles.footer)}
+            {...props}
+        >
+            <p className={styles.text}>
+                OwlTop © 2020 - {format(new Date(), 'yyyy')} Все права защищены
+            </p>
+
+            <a
+                href='#'
+                target={'_blank'}
+                className={styles.link}
+            >
+                Пользовательское соглашение
+            </a>
+            <a
+                href='#'
+                target={'_blank'}
+                className={styles.link}
+            >
+                Политика конфиденциальности
+            </a>
         </footer>
     );
 };
